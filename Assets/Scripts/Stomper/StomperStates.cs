eb3e57f49c16e1bac2ae26a03efe5f6b703630a1
Tobias Bodmer <Tobias.Bodmer@gmx.de>
2018-07-03 19:39:31 +0200

    Initial Commit

---
+++ Assets/Scripts/Stomper/StomperStates.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+
+public abstract class State
+{
+
+    public abstract void Execute();
+    public abstract State getNextState();
+
+}
+
+public class SteadyStomperState : State
+{
+    private bool SwitchWasTriggered = false;
+
+    private StomperStateMachine stateMachine;
+
+    public SteadyStomperState(StomperStateMachine stateMachine)
+    {
+        this.stateMachine = stateMachine;
+    }
+
+    public override void Execute()
+    {
+        if (!SwitchWasTriggered)
+        {
+            delayedStateChangeRequest();
+        }
+    }
+
+    public override State getNextState()
+    {
+        return new GrumbleStomperState(stateMachine);
+    }
+}
+
+public class GrumbleStomperState : State
+{
+    private StomperStateMachine stateMachine;
+    private DateTime lastStep;
+
+    public GrumbleStomperState(StomperStateMachine stateMachine)
+    {
+        this.stateMachine = stateMachine;
+    }
+
+    public override void Execute()
+    {
+        
+    }
+}
