44048dcd066aa2650a7754d6c0846aa40a45b0e4
Tobias Bodmer <Tobias.Bodmer@gmx.de>
2018-07-08 15:35:28 +0200

    first statemachine iteration.. buggy as hell

---
+++ Assets/Scripts/Stomper/StomperStateMachine.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class StomperStateMachine : MonoBehaviour
+{
+    public delegate State GetStateDelegate();
+    public double ShakeDistance = 2;
+    public double ShakeFrequency = 30;
+
+    [Tooltip("local space deviation")]
+    public Vector3 UpPoition;
+    [Tooltip("local space deviation")]
+    public Vector3 DownPosition;
+
+    [Tooltip("Time in msec to move from Up to down position")]
+    public int TimeToMoveDown = 400;
+    [Tooltip("Time in msce to move from Down to Up position")]
+    public int TimeToMoveUp = 600;
+
+    /// <summary>
+    /// time im ms
+    /// </summary>
+    public int TimeSteady = 2000;
+    /// <summary>
+    /// time in ms
+    /// </summary>
+    public int TimeGrumble = 500;
+
+    /// <summary>
+    /// time in ms
+    /// </summary>
+    public int DownTime = 200;
+
+    private State currState;
+    private GetStateDelegate nextState;
+
+    public StomperStateMachine()
+    {
+        currState = new SteadyStomperState(this);
+    }
+
+    public void ToNextState(GetStateDelegate deleg)
+    {
+        nextState = deleg;
+    }
+
+    public void Update()
+    {
+        if(nextState != null)
+        {
+            if(name == "FirstStomper")
+            {
+                Debug.Log("Changing Status");
+            }
+            currState = nextState();
+
+            nextState = null;
+        }
+        currState.Execute();
+    }
+
+    /*
+     * public void FixedUpdate()
+     * {
+     * currState.ExecuteFixed();
+     * }
+     */
+}
--- Assets/Scripts/Stomper/StomperStates.cs
+++ Assets/Scripts/Stomper/StomperStates.cs
@@ -1,51 +1,195 @@
 ﻿using System;
 using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
 
 public abstract class State
 {
+    public virtual void Execute()
+    {
 
-    public abstract void Execute();
+    }
+    public virtual void ExecuteFixed()
+    {
+
+    }
     public abstract State getNextState();
 
 }
 
-public class SteadyStomperState : State
+public abstract class StomperState : State
 {
-    private bool SwitchWasTriggered = false;
+    protected StomperStateMachine stateMachine;
+    protected bool SwitchWasTriggered = false;
 
-    private StomperStateMachine stateMachine;
-
-    public SteadyStomperState(StomperStateMachine stateMachine)
+    public StomperState(StomperStateMachine stateMachine)
     {
         this.stateMachine = stateMachine;
     }
 
     public override void Execute()
     {
+        base.Execute();
         if (!SwitchWasTriggered)
         {
-            delayedStateChangeRequest();
+            SwitchWasTriggered = true;
+            TriggerStateChange();
         }
     }
+
+    public abstract void TriggerStateChange();
+}
+
+public class SteadyStomperState : StomperState
+{
+    public SteadyStomperState(StomperStateMachine stomper) : base(stomper)
+    {
+        Debug.Log("Steady");
+    }
+
+    public override void Execute()
+    {
+        base.Execute();
+    }
+
 
     public override State getNextState()
     {
         return new GrumbleStomperState(stateMachine);
     }
+
+    public override void TriggerStateChange()
+    {
+        Task.Delay(base.stateMachine.TimeSteady).ContinueWith(t => stateMachine.ToNextState(getNextState));
+    }
 }
 
-public class GrumbleStomperState : State
+public class GrumbleStomperState : StomperState
 {
-    private StomperStateMachine stateMachine;
-    private DateTime lastStep;
+    float phaseShift = Mathf.PI / 2;
+    Vector3 origPosition;
 
-    public GrumbleStomperState(StomperStateMachine stateMachine)
+    public GrumbleStomperState(StomperStateMachine stateMachine) : base(stateMachine)
     {
-        this.stateMachine = stateMachine;
+        Debug.Log("Grumble");
+        origPosition = stateMachine.transform.localPosition;
     }
 
     public override void Execute()
     {
-        
+        base.Execute();
+
+        Vector3 toShake = new Vector3();
+        toShake.x = Mathf.Sin(Time.deltaTime * (float)stateMachine.ShakeFrequency) * (float) stateMachine.ShakeDistance - (float) stateMachine.ShakeDistance / 2;
+        toShake.y = Mathf.Sin(Time.deltaTime * (float)stateMachine.ShakeFrequency + phaseShift) * (float) stateMachine.ShakeDistance;
+        toShake.z = Mathf.Sin(Time.deltaTime * (float)stateMachine.ShakeFrequency + 2 * phaseShift) * (float) stateMachine.ShakeDistance;
+        stateMachine.transform.localPosition += toShake;
+    }
+
+    public override State getNextState()
+    {
+        stateMachine.transform.localPosition = origPosition;
+        return new StompStomperState(stateMachine);
+    }
+
+    public override void TriggerStateChange()
+    {
+        Task.Delay(base.stateMachine.TimeGrumble).ContinueWith(t => stateMachine.ToNextState(getNextState));
+
+    }
+}
+
+public class StompStomperState : StomperState
+{
+    private float downVelocity;
+    private Vector3 travelVec, rootPosition;
+    private float passedTime = 0;
+    public StompStomperState(StomperStateMachine stomper) : base(stomper)
+    {
+        Debug.Log("Stomp");
+        travelVec = stateMachine.UpPoition - stateMachine.DownPosition;
+        downVelocity = travelVec.magnitude / stateMachine.TimeToMoveDown;
+        rootPosition = stateMachine.transform.localPosition;
+    }
+
+    public override void Execute()
+    {
+        base.Execute();
+
+        passedTime += Time.deltaTime * 1000;
+        stateMachine.transform.localPosition = stateMachine.UpPoition + downVelocity * passedTime * travelVec.normalized + rootPosition;
+    }
+
+    public override State getNextState()
+    {
+        stateMachine.transform.localPosition = stateMachine.DownPosition + rootPosition;
+
+        return new DownStomperState(stateMachine);
+    }
+
+    public override void TriggerStateChange()
+    {
+        Task.Delay(base.stateMachine.TimeToMoveDown).ContinueWith(t => stateMachine.ToNextState(getNextState));
+
+    }
+}
+
+public class DownStomperState : StomperState
+{
+    private bool SwitchWasTriggered = false;
+
+    public DownStomperState(StomperStateMachine stomper) : base(stomper)
+    {
+        Debug.Log("IsDown");
+    }
+
+    public override void Execute()
+    {
+        base.Execute();
+    }
+
+
+    public override State getNextState()
+    {
+        return new GoUpStomperState(stateMachine);
+    }
+
+    public override void TriggerStateChange()
+    {
+        Task.Delay(base.stateMachine.DownTime).ContinueWith(t => stateMachine.ToNextState(getNextState));
+
+    }
+}
+
+public class GoUpStomperState : StomperState
+{
+    private float upVelocity;
+    private Vector3 travelVec, rootPosition;
+    private float passedTime = 0;
+    public GoUpStomperState(StomperStateMachine stomper) : base(stomper)
+    {
+        Debug.Log("Up");
+        rootPosition = stateMachine.transform.localPosition;
+        travelVec = stateMachine.DownPosition - stateMachine.UpPoition;
+        upVelocity = travelVec.magnitude / stateMachine.TimeToMoveUp;
+    }
+
+    public override void Execute()
+    {
+        base.Execute();
+        passedTime += Time.deltaTime * 1000;
+        stateMachine.transform.localPosition = stateMachine.DownPosition + upVelocity * passedTime * travelVec.normalized + rootPosition;
+    }
+
+    public override State getNextState()
+    {
+        stateMachine.transform.localPosition = stateMachine.UpPoition + rootPosition;
+        return new SteadyStomperState(stateMachine);
+    }
+
+    public override void TriggerStateChange()
+    {
+        Task.Delay(base.stateMachine.TimeGrumble).ContinueWith(t => stateMachine.ToNextState(getNextState));
+
     }
 }
